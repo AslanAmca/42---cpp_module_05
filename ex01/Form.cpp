b8b6015de85c6c07be774e8018a0f4bf4b2761a3
Ali Aslan <aliaslan_61@outlook.com>
2023-11-22 16:31:55 +0300

    finish

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: aaslan <aaslan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 17:09:01 by aaslan            #+#    #+#             */
-/*   Updated: 2023/11/21 15:53:13 by aaslan           ###   ########.fr       */
+/*   Updated: 2023/11/22 16:30:15 by aaslan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ Form::~Form(void)
 
 // Project Requirements
 // Form üyeleri const olduğu için Constructor Initializer List kısmında değerleri verilmelidir.
-Form::Form(const std::string name, const int gradeToSign, const int gradeToExecute) : name(name), gradeToSign(gradeToSign), gradeToExecute(gradeToExecute)
+Form::Form(const std::string name, const int gradeToSign, const int gradeToExecute) : name(name), isSigned(false), gradeToSign(gradeToSign), gradeToExecute(gradeToExecute)
 {
 	if (gradeToSign < 1 || gradeToExecute < 1)
 		throw GradeTooHighException();
@@ -84,8 +84,8 @@ const char *Form::GradeTooLowException::what() const throw()
 std::ostream &operator<<(std::ostream &output, const Form &form)
 {
 	output << "Form: " << form.getName()
-		<< ", Grade to Sign: " << form.getGradeToSign()
-		<< ", Grade to Execute: " << form.getGradeToExecute()
-		<< ", Signed: " << (form.getIsSigned() ? "Yes." : "No.");
+		   << ", Grade to Sign: " << form.getGradeToSign()
+		   << ", Grade to Execute: " << form.getGradeToExecute()
+		   << ", Signed: " << (form.getIsSigned() ? "Yes." : "No.");
 	return output;
 }
